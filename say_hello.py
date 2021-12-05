4473d3895f75a33425d5fc58ecb73fa7930ead47
RTBlanco <ronnytoribio1@hotmail.com>
2021-12-05 14:55:19 -0500

    moved parts to individual methods

--- say_hello.py
+++ say_hello.py
@@ -7,33 +7,9 @@ from kivy.uix.textinput import TextInput
 
 class SayHello(App):
   def build(self):
-    
     self._set_window()
-
-    self.window.add_widget(Image(source="logo.png"))
-    self.greetting = Label(
-      text="whats your name? ",
-      font_size=18,
-      color="#00FFCE"
-    )
-    self.window.add_widget(self.greetting)
-    
-    self.user = TextInput(
-      multiline=False,
-      padding_y=(20,20),
-      size_hint=(1,0.5)
-    )
-    self.window.add_widget(self.user)
-    
-    self.button = Button(
-      text="Greet",
-      size_hint=(1,0.5),
-      bold=True,
-      background_color="#00FFCE"
-    )
-    
-    self.button.bind(on_press=self.callback)
-    self.window.add_widget(self.button)
+    self._set_logo_and_question()
+    self._set_textinput()
     
     return self.window
   
@@ -56,6 +32,26 @@ class SayHello(App):
       color="#00FFCE"
     )
     self.window.add_widget(self.greetting)
+    
+  def _set_textinput(self):
+    self.user = TextInput(
+      multiline=False,
+      padding_y=(20,20),
+      size_hint=(1,0.5)
+    )
+    self.window.add_widget(self.user)
+    
+  def _set_button(self):
+    self.button = Button(
+      text="Greet",
+      size_hint=(1,0.5),
+      bold=True,
+      background_color="#00FFCE"
+    )
+    
+    self.button.bind(on_press=self.callback)
+    self.window.add_widget(self.button)
+  
   
 if __name__ == "__main__":
   SayHello().run()
